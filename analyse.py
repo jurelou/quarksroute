6e083a682cf58809a00e76e567405cddfb50f5bf
louis <louis@jurczyk.fr>
2019-03-29 23:11:32 -0700

    savework

--- analyse.py
+++ analyse.py
@@ -1,30 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
+import xml.etree.ElementTree as ET
+import pandas as pd
+import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-import pandas as pd
-import xml.etree.ElementTree as ET
-
-
-class XmlReader():
-	def __init__(self, filename):
-		try:
-			self.file = open(filename, 'r')
-		except FileNotFoundError:
-			raise Exception('File not found')
-
-	def parse(self):
-		data = {"header":{}, "hops":{}}
-		root = ET.parse(self.file).getroot()
-		header = root.find('header')
-		data["header"]["numQueries"] = header.get('numQueries')
-		data["header"]["queryTimeouts"] = header.get('queryTimeouts')
-		for item in root.findall('hops/entry'):
-			print(item)
-			for i in item.findall('queries/query'):
-				print("ll",i)
-		return(data)
+import matplotlib as mpl 
+from matplotlib.backend_tools import ToolBase, ToolToggleBase
+import matplotlib.patches as patches
 
 def parse(filename):
 	try:
@@ -59,7 +43,39 @@ def main(filename):
 	data = parse(filename)
 	if data is not None:
 		print(data)
-	pass
+		mpl.rcParams['toolbar'] = 'None'
+		fig = plt.figure(num='lol')
+		#fig.set_facecolor("#00000F")
+
+		"""
+		df = pd.DataFrame({ 'from':['A', 'B', 'C','A'], 'to':['D', 'A', 'E','C'] })
+		 
+		G=nx.from_pandas_edgelist(df, 'from', 'to', create_using=nx.Graph() )
+		nx.draw(G, with_labels=True, node_color='skyblue', node_size=1500, edge_color='white')
+		"""
+		G = nx.Graph()
+		i = 0
+		while i <= 10:
+			if i < 10:
+				G.add_edge(i+1,i)
+			G.add_node(i, pos=(i,2))
+			i = i +1
+
+		currentAxis = plt.gca()
+		pos=nx.get_node_attributes(G,'pos')
+		rect = patches.Rectangle((50,100),40,30,linewidth=1,edgecolor='r',facecolor='none')
+
+
+
+		nx.draw(G, pos,with_labels=True,node_size=1500, node_color="skyblue", node_shape="s", alpha=0.5, linewidths=1)
+		plt.show()
+
+
+
+
+
+
+
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		print("Usage: {} ./results.xml".format(sys.argv[0]))
