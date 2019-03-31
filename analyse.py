02f2154ac6fd66e27a624fc11557e7fc954aed3f
louis <louis@jurczyk.fr>
2019-03-31 06:45:22 -0700

    worlmap && graf

--- analyse.py
+++ analyse.py
@@ -13,16 +13,17 @@ class GUI():
 	def __init__(self, data):
 		mpl.rcParams['toolbar'] = 'None'
 		mpl.rcParams.update({'font.size': 14})
-		self.rows = ["Dns", "Minimum latency", "Average latency","Maximum latency", "qdfsl"]
+		self.rows = ["Dns", "Minimum latency", "Average latency","Maximum latency"]
 		self.data = data
 		self.cellsText = []
 		self.columns = ()
+		self.corners = (data["header"]["lowCorner"], data["header"]["highCorner"])
 
 	def addGraph(self):
 		plt.figure(num="QuarksrouteGraph")
 		self.columns = tuple([(element["ip"]) for element in self.data["hops"]])
 		minimumLat = [hop["min"] for hop in self.data["hops"]]
-		diff = [hop["max"] - hop["min"]  for hop in self.data["hops"]]
+		diff = [hop["max"] - hop["min"] + 2  for hop in self.data["hops"]]
 		b_color  = ['b'] * len(self.rows)
 		index = np.arange(len(self.columns)) + 0.3
 		plt.bar(index, diff, 0.2, bottom=minimumLat, color=b_color)
@@ -32,11 +33,10 @@ class GUI():
 		plt.figure(num="QuarksrouteGraph")
 		if self.columns:
 			plt.title('Results for: {}\n{} devices did not responded'.format(self.data["header"]["target"], self.data["header"]["queryTimeouts"]))
-			self.cellsText.append([hop["dns"] for hop in self.data["hops"]])
+			self.cellsText.append([hop["dns"][:16] for hop in self.data["hops"]])
 			self.cellsText.append(["{} {}".format(str(hop["min"]), hop["unit"]) for hop in self.data["hops"]])
 			self.cellsText.append(["{} {}".format(str(hop["avg"]), hop["unit"]) for hop in self.data["hops"]])
 			self.cellsText.append(["{} {}".format(str(hop["max"]), hop["unit"]) for hop in self.data["hops"]])
-			self.cellsText.append(["jesaispasencoreqsuoimùettre" for hop in self.data["hops"]])	
 			c_color  = ['c'] * len(self.rows)
 			table = plt.table(cellText=self.cellsText,
 			                      rowLabels=self.rows,
@@ -54,8 +54,24 @@ class GUI():
 			plt.title('No results found for: {}'.format(self.data["header"]["target"]))
 
 	def addMap(self):
-		pass
+		plt.figure(num="QuarksrouteMap")
+		m=Basemap(llcrnrlon=self.corners[0][0], llcrnrlat=self.corners[0][1],urcrnrlon=self.corners[1][0],urcrnrlat=self.corners[1][1])
+		m.drawmapboundary(fill_color='#A6CAE0', linewidth=0)
+		m.fillcontinents(color='grey', alpha=0.6, lake_color='blue')
+		m.drawcountries(color="white")
+		m.drawcoastlines(linewidth=0.1, color="white")
+		head = ()
+		index = 0
+		for hop in self.data["hops"]:
+			if hop["geo"]:
+				if head is not ():
+					m.drawgreatcircle(head[0], head[1], hop["geo"]["location"]["longitude"], hop["geo"]["location"]["latitude"], linewidth=2, color='blue')
+				head = (hop["geo"]["location"]["longitude"], hop["geo"]["location"]["latitude"])
+				m.plot(head[0], head[1], linestyle='none', marker="o", markersize=8, alpha=0.6, c="cyan", markeredgecolor="black", markeredgewidth=1)
+				plt.annotate(index,  xy=(head[0], head[1] + 2))
+				index=index+1
 	def render(self):
+		self.addMap()
 		self.addGraph()
 		self.addTable()
 		plt.show()
@@ -109,7 +125,7 @@ def getHostAddress():
 def calcStats(data):
 	reader = geolite2.reader()
 	for hop in data["hops"]:
-		hop["geo"] = reader.get(getHostAddress()) if int(hop["id"]) is 1 else reader.get(hop["ip"])
+		hop["geo"] = reader.get(getHostAddress()) if hop["id"] == "1" else reader.get(hop["ip"])
 		hop["min"] = hop["delta"] = hop["max"] = hop["avg"] = sum = 0
 		for query in hop["queries"]:
 			if hop["max"] == 0 or float(query["value"]) > hop["max"]: hop["max"] = float(query["value"])
@@ -118,12 +134,21 @@ def calcStats(data):
 			hop["unit"] = query["unit"]
 		if len(hop["queries"]) > 0: hop["avg"] = "{0:.2f}".format(sum / len(hop["queries"]))
 		hop["delta"] = (hop["max"] - hop["min"])  / 2
-	a = (data["hops"][0]["geo"]["location"]["longitude"], data["hops"][0]["geo"]["location"]["latitude"]) if len(data["hops"]) > 0 else ()
-	b = (data["hops"][1]["geo"]["location"]["longitude"], data["hops"][1]["geo"]["location"]["latitude"]) if len(data["hops"]) > 1 else ()
-	if a and b:
-		for hop in range(2, len(data["hops"])):
-			print(hop["geo"]["location"]["longitude"], ";;", hop["geo"]["location"]["latitude"])
-	print("------------",data["header"])
+	lowCorner = highCorner = (None, None)
+	for hop in data["hops"]:
+		if hop["geo"] is not None:
+			latitude = hop["geo"]["location"]["latitude"]
+			longitude = hop["geo"]["location"]["longitude"]
+			if lowCorner[0] is None or longitude < lowCorner[0]:
+				lowCorner = (longitude, lowCorner[1])
+			if lowCorner[1] is None or latitude < lowCorner[1]:
+				lowCorner = (lowCorner[0], latitude)
+			if highCorner[0] is None or longitude > highCorner[0]:
+				highCorner = (longitude, highCorner[1])
+			if highCorner[1] is None or latitude > highCorner[1]:
+				highCorner = (highCorner[0], latitude)
+	data["header"]["lowCorner"] = (lowCorner[0] - 5, lowCorner[1] - 2) 
+	data["header"]["highCorner"] = (highCorner[0] + 5, highCorner[1] + 2) 
 	return(data)
 
 def main(filename):
@@ -132,7 +157,6 @@ def main(filename):
 		data = calcStats(data)	
 		gui = GUI(data)
 		gui.render()
-
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
